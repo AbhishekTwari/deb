387bb34601abf600d0bab8873eb624b1b760e88a
AbhishekTwari <111764092+AbhishekTwari@users.noreply.github.com>
2025-03-02 02:55:33 +0530

    Add files via upload

---
+++ SpringBootCloudEmailService/src/test/java/com/example/SpringBootCloudEmailService/SpringBootCloudEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCloudEmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
